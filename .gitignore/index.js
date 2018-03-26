2694e444fa74d2bd3d19a51395e81abbcf6964e5
Kuzaakii <37170699+Kuzaakii@users.noreply.github.com>
2018-03-26 18:52:44 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,93 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "¤"
+
+client.on('ready', () => {
+	client.user.setGame('¤help - kuzakiibot.com');
+  console.log('Je suis prêt !');
+});
+
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  guild.channels.get("420314176020545548").send(`${member.user} a rejoint !`);
+});
+
+client.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  guild.channels.get("420314176020545548").send(`${member.user.username} a quitté !`);
+});
+
+client.on('message', message => {
+  if (message.content === 'Salut') {
+    message.reply('salut :p');
+  }
+   if(message.content.startsWith(prefix + "avatar")){
+    var mentionned = message.mentions.users.first();
+    if(mentionned){
+        var avatar = mentionned.avatarURL;
+    } else {
+        var avatar = message.author.avatarURL;
+    }
+    message.channel.send("", {embed: {
+      color: 0xFF7777,
+      image: {
+        url:avatar,
+}}})}
+ if (message.content.startsWith("¤serverinfo")){
+    var embed1 = new Discord.RichEmbed()
+    .setTitle("ServerInformaion")
+    .addField("Server Name", message.guild.name)
+    .addField("users", client.users.size)
+    .addField("channels", client.channels.size)
+    .addField("region", message.guild.region)
+    .addField("creation at", message.guild.createdAt)
+    .addField("Owner Of Server", message.guild.owner)
+    .addField("Roles", message.guild.roles.size)
+    .addField("users", client.users.size)
+    .setFooter("This Will Updated from time to other")
+    .setThumbnail(message.guild.iconURL)
+    message.channel.send(embed1);
+  }
+if ( message.content === (prefix + "help")){
+    message.channel.send("", {embed: {
+    color: 0xFF7777,
+    title: "Commande:",
+    description: `prefix : ${prefix}`,
+    fields: [{
+      name: "Utilitaire:",
+      value: `${prefix}avatar - Permet de voir votre avatar ou celui d'un utilisateur mentionné.\n${prefix}invite - Rejoindre mon serveur.\n${prefix}help - Affiche la liste de commandes.\n*help - Permet d'utiliser le bot musique. \n${prefix}serverinfo - Donnes des informations sur le serveur.`  ,
+	  
+    },{
+      name: "Fun:",
+      value: `${prefix}chien - Envoie des images aléatoires de chiens.\n${prefix}chat - Envoie des images aléatoires de chats`,
+}]}})} 
+    if(message.content === (prefix + 'invite')){
+		var InvitePerm = ['ADMINISTRATOR', 'MANAGE_ROLES', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_MESSAGES'];
+		client.generateInvite(InvitePerm)
+		  .then(link => {
+			  message.channel.send({
+				  embed: {
+					  type: 'rich',
+					  color: 0xFF7777,
+					  title: 'Invitation',
+					  description: `[Invite](${link})`,
+}})})} 
+if(message.content === prefix + "chien" ){
+  const randomPuppy = require("random-puppy");
+  randomPuppy().then(chien => {
+    message.channel.send("", {embed: {
+      color: 0xFF7777,
+      image: {
+        url: chien,
+}}})})} 
+
+ if( message.content === prefix + "chat" ) {
+  const randomCat = require("random-cat");
+    message.channel.send("", {embed: {
+      color: 0xFF7777,
+      image: {
+        url: randomCat.get(),
+    }}})};
+    });
+
+client.login("NDI2NDU3MzAzODY5NTU0Njk4.DZWZSw.wZQ1IMfJXYnn7SgTQRPeswXEGFc");
