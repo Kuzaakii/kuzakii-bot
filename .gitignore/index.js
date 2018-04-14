3131665abad82d6ee8a0ff4290084568824b12f0
Kuzaakii <37170699+Kuzaakii@users.noreply.github.com>
2018-04-14 15:26:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,22 +1,93 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "÷"
+const prefix = "+"
 
+// Team Aqua
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434691524417290241") {
+    messageReaction.message.guild.members.get(user.id).addRole("420634223997747211");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434691524417290241") {
+    messageReaction.message.guild.members.get(user.id).removeRole("420634223997747211");
+  }
+});
+// Team Rocket
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434696132422008843") {
+    messageReaction.message.guild.members.get(user.id).addRole("434689676519931904");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434696132422008843") {
+    messageReaction.message.guild.members.get(user.id).removeRole("434689676519931904");
+  }
+});
+// Team Magma
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434695319783997440") {
+    messageReaction.message.guild.members.get(user.id).addRole("434693665080737796");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434695319783997440") {
+    messageReaction.message.guild.members.get(user.id).removeRole("434693665080737796");
+  }
+});
+// Team Galaxie 
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697176027758593") {
+    messageReaction.message.guild.members.get(user.id).addRole("420634390234660868");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697176027758593") {
+    messageReaction.message.guild.members.get(user.id).removeRole("420634390234660868");
+  }
+});
+// Team Plasma
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697844650278912") {
+    messageReaction.message.guild.members.get(user.id).addRole("434694027246567424");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697844650278912") {
+    messageReaction.message.guild.members.get(user.id).removeRole("434694027246567424");
+  }
+});
+// Team Flare
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697845241806869") {
+    messageReaction.message.guild.members.get(user.id).addRole("434694392310398976");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697845241806869") {
+    messageReaction.message.guild.members.get(user.id).removeRole("434694392310398976");
+  }
+});
+// Team Skull
+client.on('messageReactionAdd', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697844910194689") {
+    messageReaction.message.guild.members.get(user.id).addRole("434694666835722240");
+  }
+});
+client.on('messageReactionRemove', (messageReaction, user) => {
+  if (messageReaction.emoji.id == "434697844910194689") {
+    messageReaction.message.guild.members.get(user.id).removeRole("434694666835722240");
+  }
+});
+
+
+// Ready 
 client.on('ready', () => {
-	client.user.setGame('÷help - kuzakiibot.com');
+	client.user.setGame('+help - kuzakiibot.com');
   console.log('Je suis prêt !');
 });
 
-client.on('guildMemberAdd', member => {
-  let guild = member.guild;
-  guild.channels.get("420314176020545548").send(`${member.user} a rejoint !`);
-});
-
-client.on('guildMemberRemove', member => {
-  let guild = member.guild;
-  guild.channels.get("420314176020545548").send(`${member.user.username} a quitté !`);
-});
-
+// Salut 
 client.on('message', message => {
   if (message.content === 'Salut') {
     message.reply('salut :p');
@@ -33,20 +104,25 @@ client.on('message', message => {
       image: {
         url:avatar,
 }}})}
- if (message.content.startsWith(prefix + "serverinfo")){
+ if (message.content.startsWith("¤serverinfo")){
     var embed1 = new Discord.RichEmbed()
-    .setTitle("Information du Serveur")
-    .addField("Nom du Serveur", message.guild.name)
-    .addField("Salon", client.channels.size)
-    .addField("Région", message.guild.region)
-    .addField("Créer le", message.guild.createdAt)
-    .addField("Créateur", message.guild.owner)
+    .setTitle("ServerInformaion")
+    .addField("Server Name", message.guild.name)
+    .addField("users", client.users.size)
+    .addField("channels", client.channels.size)
+    .addField("region", message.guild.region)
+    .addField("creation at", message.guild.createdAt)
+    .addField("Owner Of Server", message.guild.owner)
     .addField("Roles", message.guild.roles.size)
-    .addField("Utilisateurs", client.users.size)
-    .setFooter("Kυzαkıı-вσт © •")
+    .addField("users", client.users.size)
+    .setFooter("This Will Updated from time to other")
     .setThumbnail(message.guild.iconURL)
     message.channel.send(embed1);
   }
+         if (message.content.startsWith(prefix +"say")) {
+                    message.delete(message.author)
+                    message.channel.send(message.content.substr(prefix.length+4))
+            }
 if ( message.content === (prefix + "help")){
     message.channel.send("", {embed: {
     color: 0xFF7777,
@@ -54,21 +130,21 @@ if ( message.content === (prefix + "help")){
     description: `prefix : ${prefix}`,
     fields: [{
       name: "Utilitaire:",
-      value: `${prefix}avatar - Permet de voir votre avatar ou celui d'un utilisateur mentionné.\n${prefix}support - Rejoindre le serveur pour avoir de l'aide.\n${prefix}help - Affiche la liste de commandes.\n*help - Permet d'utiliser le bot musique. \n${prefix}serverinfo - Donnes des informations sur le serveur.`  ,
+      value: `${prefix}avatar - Permet de voir votre avatar ou celui d'un utilisateur mentionné.\n${prefix}invite - Rejoindre mon serveur.\n${prefix}help - Affiche la liste de commandes.\n*help - Permet d'utiliser le bot musique. \n${prefix}serverinfo - Donnes des informations sur le serveur.`  ,
 	  
     },{
       name: "Fun(OFF):",
-      value: `${prefix}chien - Envoie des images aléatoires de chiens.\n${prefix}chat - Envoie des images aléatoires de chats`,
+      value: `${prefix}chien (OFF) - Envoie des images aléatoires de chiens.\n${prefix}chat - Envoie des images aléatoires de chats`,
 }]}})} 
-    if(message.content === (prefix + 'support')){
+    if(message.content === (prefix + 'invite')){
 		var InvitePerm = ['ADMINISTRATOR', 'MANAGE_ROLES', 'KICK_MEMBERS', 'BAN_MEMBERS', 'MANAGE_MESSAGES'];
-		client.guilds.get("418518151861370890").channels.get("418564457795551236").createInvite()
+		client.generateInvite(InvitePerm)
 		  .then(link => {
 			  message.channel.send({
 				  embed: {
 					  type: 'rich',
 					  color: 0xFF7777,
-					  title: 'Invitation',
+					  title: 'Clique ici pour rejoindre mon serveur !',
 					  description: `[Invite](${link})`,
 }})})} 
 if(message.content === prefix + "chien" ){
